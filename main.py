0e86396f51c6b23599b762e1fd67776841089a0e
simonzsy <46257084+simonzsy@users.noreply.github.com>
2021-03-05 17:39:14 +0800

    Update and rename demo.py to main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import sys
+import torch
+from face_detector import *
+from face_landmark import *
+import os
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/')
+
+
+def image_run():
+    face_detector_handle = FaceDetector()
+    face_landmark_handle = FaceLandmark()
+
+    image = cv2.imread('data/1.jpg')
+    detections, _ = face_detector_handle.run(image)
+
+    face_detector_handle.show_result(image, detections)
+
+    if len(detections) == 0:
+        return
+
+    for detection in detections:
+        landmarks, states = face_landmark_handle.run(image, detection)
+        # face_landmark_handle.show_result(image, landmarks)
+        return "landmarks ok"
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
